69bba362f892635a18c3ba5f1141ad4a18d402dc
the1812 <the1812@outlook.com>
2021-03-16 16:08:32 +0800

    Initial commit

---
+++ SteamCSS/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamCSS
+{
+  class Program
+  {
+    static void Main()
+    {
+      try
+      {
+        var steamPath = File.ReadAllText("SteamPath.txt").Trim();
+        var style = File.ReadAllText("Style.css");
+        var cssFiles = new List<string> { "steamui/css/main.css", "resource/webkit.css" };
+        cssFiles.ForEach(file =>
+        {
+          var filePath = Path.Combine(steamPath, file);
+          if (!File.Exists(filePath))
+          {
+            return;
+          }
+          var content = File.ReadAllText(filePath);
+          if (!content.StartsWith(style))
+          {
+            File.WriteAllText(filePath, style + "\n" + content); 
+          }
+        });
+      }
+      catch (FileNotFoundException ex)
+      {
+        Console.WriteLine(ex.Message);
+      }
+    }
+  }
+}
