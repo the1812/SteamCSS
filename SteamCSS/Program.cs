989cf61c7b21444ec0e30c51499baf98cf278aed
the1812 <the1812@outlook.com>
2021-11-28 14:53:10 +0800

    Add style path file

--- SteamCSS/Program.cs
+++ SteamCSS/Program.cs
@@ -4,18 +4,21 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace SteamCSS
 {
   class Program
   {
+    [STAThread]
     static void Main()
     {
       try
       {
         var steamPath = File.ReadAllText("SteamPath.txt").Trim();
         var style = File.ReadAllText("Style.css");
-        var cssFiles = new List<string> { "steamui/css/main.css", "resource/webkit.css" };
+        var cssFiles = File.ReadAllText("StylePath.txt").Trim().Split(new string [] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        var processedFiles = new List<string>();
         cssFiles.ForEach(file =>
         {
           var filePath = Path.Combine(steamPath, file);
@@ -26,13 +29,16 @@ namespace SteamCSS
           var content = File.ReadAllText(filePath);
           if (!content.StartsWith(style))
           {
-            File.WriteAllText(filePath, style + "\n" + content); 
+            File.WriteAllText(filePath, style + Environment.NewLine + content);
+            processedFiles.Add(filePath);
           }
         });
+        var message = string.Join(Environment.NewLine, processedFiles);
+        MessageBox.Show(string.IsNullOrWhiteSpace(message) ? "没有要处理的文件" : $"已处理:{Environment.NewLine}{message}", "SteamCSS", MessageBoxButtons.OK, MessageBoxIcon.Information);
       }
       catch (FileNotFoundException ex)
       {
-        Console.WriteLine(ex.Message);
+        MessageBox.Show(ex.Message, "SteamCSS", MessageBoxButtons.OK, MessageBoxIcon.Error);
       }
     }
   }
